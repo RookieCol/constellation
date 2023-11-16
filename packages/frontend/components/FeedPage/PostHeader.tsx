a8d4c9f5d74dd9bedee32cdaf4dc0b148ce55f4f
chris <christian.arevalo.fonseca@gmail.com>
2023-11-16 22:29:24 +0000

    :sparkles: Implemented post cards

---
+++ packages/frontend/components/FeedPage/PostHeader.tsx
@@ -0,0 +1,12 @@
+export default function PostHeader({ userHandle }: { userHandle: string }): React.ReactElement {
+    return (
+        <div className="flex justify-start items-center gap-3">
+            <div className="avatar">
+                <div className="w-10 md:w-12 bg-green400 rounded-full">
+                    {/* <Image /> */}
+                </div>
+            </div>
+            <h2 className="font-semibold">@{userHandle}</h2>
+        </div>
+    )
+}
