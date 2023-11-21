68ccc023e5c7394d75e13d03653b04f56a09dcf9
Santiago Viana <salviega6@gmail.com>
2023-11-21 09:27:55 -0500

    :sparkles: Was added Treasury contract

---
+++ packages/Hardhat/contracts/Treasury.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Treasury is Ownable {
+	// ************************ //
+	// *       Treasury       * //
+	// ************************ //
+
+	receive() external payable {}
+
+	function withdraw() external onlyOwner {
+		require(address(this).balance > 0, 'withdraw: no funds to withdraw');
+
+		(bool success, ) = msg.sender.call{value: address(this).balance}('');
+		require(success, 'withdraw: failed to send Ether');
+	}
+
+	// ************************ //
+	// *  Getters y Setters   * //
+	// ************************ //
+
+	function getBalance() public view returns (uint) {
+		return address(this).balance;
+	}
+}
